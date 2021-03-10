6bd7132915635f4b93b59d202997f4500c3aca70
Josh Ourisman <me@josho.io>
2021-03-10 21:48:33 +0000

    Added some basic settings.

--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -1,1 +1,61 @@
+from typing import List
+
 from pydantic_settings import PydanticSettings
+
+class {{ project_name }}Settings(PydanticSettings):
+    INSTALLED_APPS: List[str] = (
+        ["whitenoise.runserver_nostatic"]
+        + [
+            "django.contrib.admin",
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.messages",
+            "django.contrib.sessions",
+            "django.contrib.staticfiles",
+        ]
+        + ["django_extensions"]
+        + []
+    )
+
+    MIDDLEWARE: List[str] = [
+        "django.middleware.security.SecurityMiddleware",
+        "whitenoise.middleware.WhiteNoiseMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    ]
+
+    TEMPLATES: List[dict] = [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [],
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "context_processors": [
+                    "django.template.context_processors.debug",
+                    "django.template.context_processors.request",
+                    "django.contrib.auth.context_processors.auth",
+                    "django.contrib.messages.context_processors.messages",
+                ],
+            },
+        },
+    ]
+
+    STATIC_URL: str = "/static/"
+
+    LOGGING: dict = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "class": "rich.logging.RichHandler",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "WARNING",
+        },
+    }
